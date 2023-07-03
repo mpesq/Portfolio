fc06e8f10f6e7d7d93d382297e92691f3e334969
marcp <marc.pesquera@gmail.com>
2023-07-04 01:14:17 +0200

    Components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hobbies from "./components/Hobbies/Hobbies";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Hobbies></Hobbies>
+            </header>
+        </div>
+    );
+}
+
+export default App;
